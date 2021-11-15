2dab4d15bf65aa6608d3af0171a55ab9814987ef
tusharchaudhari30 <chaudharitushar2077@gmail.com>
2021-11-16 01:03:43 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,8 @@
 sudo apt -y update
 sudo apt -y upgrade
 wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash
-sudo apt -y install extrepo
-sudo extrepo enable jellyfin
+sudo apt -y install apt-transport-https gnupg lsb-release
+wget -O - https://repo.jellyfin.org/debian/jellyfin_team.gpg.key | sudo apt-key add -
+echo "deb [arch=amd64] https://repo.jellyfin.org/debian ubuntu main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
 sudo apt update
 sudo apt install jellyfin
