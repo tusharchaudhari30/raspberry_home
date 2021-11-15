5a82d898d694b1b6dea87d6bcc418fe144fe0383
tusharchaudhari30 <chaudharitushar2077@gmail.com>
2021-11-15 23:58:23 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update
-sudo apt upgrade
+sudo apt -y update
+sudo apt -y upgrade
 wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash
 sudo reboot
