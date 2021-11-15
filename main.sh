4891e128576dbaab1e62a73f0deb01c93580c954
tusharchaudhari30 <chaudharitushar2077@gmail.com>
2021-11-16 01:16:28 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,6 +4,6 @@ sudo apt -y upgrade
 wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash
 sudo apt -y install apt-transport-https gnupg lsb-release
 wget -O - https://repo.jellyfin.org/debian/jellyfin_team.gpg.key | sudo apt-key add -
-echo "deb [arch=amd64] https://repo.jellyfin.org/debian ubuntu main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
+echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/ubuntu $( lsb_release -c -s ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
 sudo apt update
 sudo apt -y install jellyfin
